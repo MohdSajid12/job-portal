adf80e7894781d3002615b3e87c62281f9345763
sajid shaikh <sajidsultan312@gmail.com>
2025-06-08 12:39:14 +0530

    Initial Setup of job portal and Design or schema

---
+++ models/companyModel.js
@@ -0,0 +1,28 @@
+import mongoose, { mongo } from "mongoose";
+
+const companySchema = mongoose.Schema({
+
+     name : {
+        type :String,
+        required :true
+     },
+     description :{
+        type :String,
+     },
+     website : {
+        type :String,
+     },
+      location :{
+        type :String,
+     },
+     logo :{
+        type : String    //URL OF COMPANY LOGO
+     },
+     userId :{
+       type : mongoose.Schema.Types.ObjectId,
+       ref :'User',
+       require :true
+     }
+} , {timestamps :true});
+
+export const Company = mongoose.Model('Company' ,companySchema);
